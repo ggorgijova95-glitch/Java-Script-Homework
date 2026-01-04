013f808ff4c56431d5be92a2f8af5a2601226eee
ggorgijova95-glitch <ggorgijova95@gmail.com>
2026-01-04 18:19:11 +0100

    changes

--- Object animal/object-animal.js
+++ Object animal/object-animal.js
@@ -1,11 +1,10 @@
-//# Homework 4
-
-//## Part 1.Create OBJECT animal with 2 properties and 1 method:
-//name
-//kind
-//speak (method)
-//this method will take one parameter and will print in the console a message: e.g. dog.speak(“hey bro!!!”) will log in the console “Dog says: ‘Hey bro!!!’”
-//Bonus: enter the values from prompt
+/*# Homework 4
+Part 1.Create OBJECT animal with 2 properties and 1 method:
+name
+kind
+speak (method)
+this method will take one parameter and will print in the console a message: e.g. dog.speak(“hey bro!!!”) will log in the console “Dog says: ‘Hey bro!!!’”
+Bonus: enter the values from prompt*/
 
 //my solution
 // Ask the user for animal details
