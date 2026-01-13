f24c54346e9bd0106b62c3513b7728dba242efe9
ggorgijova95-glitch <ggorgijova95@gmail.com>
2026-01-13 12:35:25 +0100

    changes

---
+++ winter break exercise/exercise-1.js
@@ -0,0 +1,46 @@
+// 1. Number
+let age = 23;
+console.log(typeof age);
+console.log(age);
+
+// 2. String
+let user = "Gabriela";
+console.log(typeof user);
+console.log(user);
+
+// 3. Boolean
+let isStudent = true;
+console.log(typeof isStudent);
+console.log(isStudent);
+
+// 4. Undefined
+let city;
+console.log(typeof city);
+console.log(city);
+
+// 5. Null
+let car = null;
+console.log(typeof car);
+console.log(car)
+
+// 6. Array
+let colors = ["red", "green", "blue"];
+console.log(typeof colors);
+console.log(colors)
+
+// 7. Object
+let person = {
+    firstName: "Gabriela",
+    age: 23
+};
+console.log(typeof person);
+console.log(person)
+
+// 8. Function
+function sayHello() {
+    return "Hello!";
+}
+console.log(typeof sayHello);
+console.log(sayHello)
+
+alert("Check the console for variable outputs!");
