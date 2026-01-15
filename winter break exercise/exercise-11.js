f68577bee091bd949de49890eb196809541fafb1
ggorgijova95-glitch <ggorgijova95@gmail.com>
2026-01-15 11:53:57 +0100

    changes

--- winter break exercise/exercise-11.js
+++ winter break exercise/exercise-11.js
@@ -4,11 +4,28 @@
   let result = [];
   //function to transform the array based on odd/even rules
 function transformArrayWithPrompt() {
+  if (count <= 0) {
+    alert("Please enter a valid positive number for the count.Try again.");
+    return;
+  }
+  else if (isNaN(count)) {
+    alert("Please enter a valid number.Try again.");
+    return;
+  }
   // Get numbers from the user
   for (let i = 0; i < count; i++) {
     let num = Number(prompt("Enter number " + (i + 1)));
+    if (num >= 100) {
+      alert("Please enter a number less than 100.Try again.");
+      return;
+    }
+    else if (isNaN(num)) {
+      alert("Please enter a valid number.Try again.");
+      return;
+    }
     arr.push(num);
   }
+  
   
   // Transform the numbers
   for (let i = 0; i < arr.length; i++) {
@@ -22,6 +39,7 @@ function transformArrayWithPrompt() {
   }
 
   // Display the original and transformed arrays
+  
   console.log("Original array:", arr);
   console.log("Transformed array:", result);
 }
